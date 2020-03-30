4b8e5bd80331d31774d17f3f27190a3c562fbe7a
sanjvij <62745844+sanjvij@users.noreply.github.com>
2020-03-30 16:51:03 +0100

    Fibonnaci series

---
+++ Fibonacci/Fibonacci.Test/FibonacciTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fibonacci.Test
+{
+    class FibonacciTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void FibonacciTest1()
+        {
+            string expected = "0,1,1,2,3,5,8,13,21,34,55,89,144";
+            int n = 13;
+            int startpos1 = 0;
+            int startpos2 = 1;
+
+            string output = Fibonacci.GetFibonacciSeries(startpos1, startpos2, n);
+            Console.WriteLine(output);
+            Assert.AreEqual(expected, output);
+        }
+
+        [Test]
+        public void FibonacciTest2()
+        {
+            string expected = "4,5,9,14,23";
+            int n = 5;
+            int startpos1 = 4;
+            int startpos2 = 5;
+
+            string output = Fibonacci.GetFibonacciSeries(startpos1, startpos2, n);
+            Console.WriteLine(output);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci/Fibonacci.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fibonacci
+{
+    public static class Fibonacci
+    {
+        public static string GetFibonacciSeries(int pos1, int pos2, int n)
+        {
+            string output = string.Empty;
+            int startpos1 = pos1;
+            int startpos2 = pos2;
+            string COMMA = ",";
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0)
+                {
+                    output += startpos1;
+                    continue;
+                }
+
+                if (i == 1)
+                {
+                    output += COMMA + startpos2;
+                    continue;
+                }
+
+                int sum = startpos1 + startpos2;
+                output += COMMA + sum;
+
+                startpos1 = startpos2;
+                startpos2 = sum;
+               
+            }
+
+            return output;
+        }
+
+    }
+}
