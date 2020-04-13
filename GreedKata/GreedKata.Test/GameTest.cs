fb8662b53b217a7ee98c3a57489aa81b08a1fabe
sanjvij <62745844+sanjvij@users.noreply.github.com>
2020-04-13 18:46:07 +0100

    A single one (100 points)

--- GreedKata/GreedKata.Test/GameTest.cs
+++ GreedKata/GreedKata.Test/GameTest.cs
@@ -1,4 +1,6 @@
+using GreedKata;
 using NUnit.Framework;
+using System;
 
 namespace Tests
 {
@@ -10,9 +12,34 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void SingleOne100PointsScenario1()
         {
-            Assert.Pass();
+            Game greed = new Game();
+            int actual = greed.RollDice(DiceValue.One, DiceValue.Two, DiceValue.Three, DiceValue.Four, DiceValue.Five, DiceValue.Six);
+            int expected = 100;
+            Console.WriteLine($"Expected : {expected}  Actual : {actual}");
+            Assert.AreEqual(expected, actual);
         }
+
+        [Test]
+        public void SingleOne100PointsScenario2()
+        {
+            Game greed = new Game();
+            int actual = greed.RollDice(DiceValue.One, DiceValue.Two, DiceValue.One, DiceValue.Four, DiceValue.One, DiceValue.Six);
+            int expected = 300;
+            Console.WriteLine($"Expected : {expected}  Actual : {actual}");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SingleOne100PointsScenario3()
+        {
+            Game greed = new Game();
+            int actual = greed.RollDice(DiceValue.Three, DiceValue.Two, DiceValue.Three, DiceValue.Four, DiceValue.Three, DiceValue.Six);
+            int expected = 0;
+            Console.WriteLine($"Expected : {expected}  Actual : {actual}");
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
--- GreedKata/GreedKata/Game.cs
+++ GreedKata/GreedKata/Game.cs
@@ -1,8 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GreedKata
 {
+    public enum DiceValue
+    {
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6
+    }
+
     public class Game
     {
+        Dictionary<DiceValue, int> pointSystem = new Dictionary<DiceValue, int>()
+        {
+            { DiceValue.One, 100 },
+            { DiceValue.Two, 0 },
+            { DiceValue.Three, 0 },
+            { DiceValue.Four, 0 },
+            { DiceValue.Five, 0 },
+            { DiceValue.Six , 0 },
+        };
+
+        public int RollDice(DiceValue v1, DiceValue v2, DiceValue v3, DiceValue v4, DiceValue v5, DiceValue v6)
+        {
+            int score = 0;
+            score += pointSystem.GetValueOrDefault(v1);
+            score += pointSystem.GetValueOrDefault(v2);
+            score += pointSystem.GetValueOrDefault(v3);
+            score += pointSystem.GetValueOrDefault(v4);
+            score += pointSystem.GetValueOrDefault(v5);
+            score += pointSystem.GetValueOrDefault(v6);
+            return score;
+        }
     }
 }
